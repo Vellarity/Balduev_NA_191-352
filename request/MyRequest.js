445c3168ab7409991421f1958b3195ea6756f00c
Nikita Balduev <Nikitaaorta@ya.ru>
2021-04-05 23:55:16 +0300

    Lab3 0.9
    Signed-off-by: Nikita Balduev <Nikitaaorta@ya.ru>

--- request/MyRequest.js
+++ request/MyRequest.js
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
 import { Button } from 'react-native';
 import { Text, View, ScrollView } from 'react-native';
-import HTML from "react-native-render-html";
+import { parse } from 'node-html-parser';
 
 
 export const MyRequest = props =>{
 
-    const [data, setData] = useState('<h1> Нажмите GET, чтобы получить страницу</h1>');
+    const [data, setData] = useState('<h1>Нажмите GET, чтобы получить страницу</h1>');
+    const [parseData,setParse] = useState('')
     const getMoviesFromApiAsync = async () => {
         try {
           let response = await fetch(
-            'http://www.yandex.ru'
+            'https://cbr.ru/'
           );
           let html = await response.text();
           setData(html);
+          const root = parse(html);
+          console.log(root.querySelector('.main-indicator_info').text + " " + root.querySelector('.main-indicator_value').text);
+          setParse(root.querySelector('.main-indicator_info').text + " " + root.querySelector('.main-indicator_value').text)
           return html;
         } catch (error) {
           console.error(error);
         }
       };
-
   return (
     <View style={{ flex: 1, }}>
         <ScrollView style={{ flex: 1 }}>
-            <HTML source={{ html: data }} />
+            <Text style={{color:"#FFF"}}>{data}</Text>
         </ScrollView>
+        <Text style={{borderTopColor:"#FFF" , borderTopWidth:2, color:"#FFF", fontSize:14}}>{parseData}</Text>
         <Button title="Get" onPress={getMoviesFromApiAsync} color="#7289DA" />
     </View>
   );
